6f6229b387b6a75b516f6a5dc0f0ce6952f70d89
xcnkx <cnk_2806@me.com>
2018-11-14 01:42:08 +0900

    text changed

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="がんばれ！！七海！！大好きだよ"))
 
 if __name__ == "__main__":
     app.run()
