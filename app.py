e466f91c4a8ae629a6547966b2d96bf2ce70d16d
xcnkx <cnk_2806@me.com>
2018-11-14 01:26:02 +0900

    python ver. changed

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ from linebot.models import (
 app = Flask(__name__)
 
 # 環境変数からchannel_secret・channel_access_tokenを取得
-channel_secret = os.environ['LINE_CHANNEL_SECRET']
-channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+channel_secret = os.environ['CHANNEL_SECRET']
+channel_access_token = os.environ['CHANNEL_ACCESS_TOKEN']
 
 if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    print('Specify CHANNEL_SECRET as environment variable.')
     sys.exit(1)
 if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    print('Specify CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
