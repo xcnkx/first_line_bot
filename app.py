6ee9a177e15382fc72b4103e0fbae1139d8f1a76
xcnkx <cnk_2806@me.com>
2018-11-15 23:02:28 +0900

    add janome

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def handle_message(event):
         words += i+" "
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=i))
+        TextSendMessage(text=words))
 
 if __name__ == "__main__":
     app.run()
