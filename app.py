7469747ac75b5ed952b31d6eee9058a9192d764a
xcnkx <cnk_2806@me.com>
2018-11-15 23:02:09 +0900

    add janome

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def handle_message(event):
 
     words = []
     for i in tokens:
-    words += i+" "
+        words += i+" "
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=i))
